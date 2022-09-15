c9face8d86c82c29d43c5535d54622e59f4dc34f
Sumin <choesumin@choesumin-ui-MacBookAir.local>
2022-09-15 16:58:55 +0900

    4_TextViewTest

---
+++ 4_TextViewTest/app/src/main/java/com/example/androidfirst/MainActivity.java
@@ -0,0 +1,29 @@
+//package com.example.androidfirst;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        TextView myText = new TextView(this);
+//        myText.setText("코드로 문자열 출력하기");
+//        setContentView(myText);
+//    }
+//}
+
+package com.example.androidfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
