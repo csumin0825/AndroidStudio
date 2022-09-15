5149335f528de060ffe42b931b75333fd69f2464
Sumin <choesumin@choesumin-ui-MacBookAir.local>
2022-09-15 18:01:46 +0900

    4_EditTextTest_1

---
+++ 4_EditTextTest/app/src/main/java/com/example/edittexttext/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.edittexttext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+    protected void inCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                EditText edit = (EditText)findViewById(R.id.edit);
+                String str = edit.getText().toString();
+                Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
