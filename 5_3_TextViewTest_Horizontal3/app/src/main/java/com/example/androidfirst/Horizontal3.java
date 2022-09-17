312d42b6ab2fd4b75f6c9be637785de8d629039c
sumin <csumin008255@gmail.com>
2022-09-17 15:35:16 +0900

    5_3_Horizontal3

---
+++ 5_3_TextViewTest_Horizontal3/app/src/main/java/com/example/androidfirst/Horizontal3.java
@@ -0,0 +1,11 @@
+package com.example.androidfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Horizontal3 extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.horizontal3);
+    }
+}
