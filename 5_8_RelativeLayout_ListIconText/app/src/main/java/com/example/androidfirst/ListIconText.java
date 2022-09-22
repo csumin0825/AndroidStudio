e97fef0336826a17c6b1a0d453b4ab531578d304
sumin <csumin008255@gmail.com>
2022-09-22 15:26:58 +0900

    5_8_RelativeLayout_ListIconText

---
+++ 5_8_RelativeLayout_ListIconText/app/src/main/java/com/example/androidfirst/ListIconText.java
@@ -0,0 +1,11 @@
+package com.example.androidfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListIconText extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listicontext);
+    }
+}
