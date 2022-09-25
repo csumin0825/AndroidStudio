cc3bb6d96c8e0ecc825536679bfc78fc639c51de
sumin <csumin008255@gmail.com>
2022-09-25 17:48:28 +0900

    6_1_LinearLayout

---
+++ 6_1_NestLayout/app/src/main/java/com/example/androidfirst/NestLayout.java
@@ -0,0 +1,11 @@
+package com.example.androidfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NestLayout extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table);
+    }
+}
